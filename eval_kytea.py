0fc9c83b5a8e36eb6ec0f12196bce8b589a47c85
shigashiyama <mapion.benri@gmail.com>
2017-06-16 15:29:02 +0900

    update script for kytea

--- eval_kytea.py
+++ eval_kytea.py
@@ -1,68 +1,55 @@
+import argparse
 import subprocess
 from subprocess import PIPE
 
 import util
+import sys
 from conlleval import conlleval
 
 
-def run_and_eval_kytea(in_path, out_path):
-    print("Read file:", in_path)
+def train_kytea(train_path, model_path, dict_path):
+    cmd = 'train-kytea -full ' + train_path + ' -model ' + model_path
+    if dict_path:
+        cmd += ' -dict ' + dict_path
+    p = subprocess.Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+    out, err = p.communicate()
+    print(out.decode('utf-8').rstrip())
+    return
 
+
+def eval_kytea(gold_path, pred_path='', model_path=''):
     sen_count = 0
-    sen = ''
-    with open(in_path) as fi, open(out_path, 'w') as fo:
-        bof = True
-        g_chars = []
-        g_bounds = []
-        eval_counts = None
-        
-        for line in fi:
-            line = line.rstrip('\n').split('\t')
-            bos = line[0]
-            word = line[1]
-            
-            if not bof and bos == 'B':
-                sen_count += 1
+    eval_counts = None
 
-                # run kytea on the previous sentence and evaluate it
-                k_res, k_bounds = run_kytea_for_word_segmentation(sen)
-                iter = generate_lines(g_chars, g_bounds, k_bounds)
-                eval_counts = conlleval.merge_counts(eval_counts, conlleval.evaluate(iter))
+    for g_chars, g_bounds, k_bounds in sentence_iterator(
+            gold_path, pred_path=pred_path, model_path=model_path):
 
-                if sen_count % 20 == 0:
-                    c = eval_counts
-                    acc = conlleval.calculate_accuracy(c.correct_tags, c.token_counter)
-                    overall = conlleval.calculate_metrics(c.correct_chunk, c.found_guessed, c.found_correct)
-                    print('end: %d sentences' % sen_count)
-                    print('#sen, #token, #chunk, #chunk_pred: %d %d %d %d' %
-                          (sen_count, c.token_counter, c.found_correct, c.found_guessed))
-                    print('TP, FP, FN: %d %d %d' % (overall.tp, overall.fp, overall.fn))
-                    print('A, P, R, F:%6.2f %6.2f %6.2f %6.2f' % 
-                          (100.*acc, 100.*overall.prec, 100.*overall.rec, 100.*overall.fscore))
-                    print()
+        iter = generate_lines(g_chars, g_bounds, k_bounds)
+        eval_counts = conlleval.merge_counts(eval_counts, conlleval.evaluate(iter))
+        sen_count += 1
 
-                # log
+        if sen_count % 50000 == 0:
+            c = eval_counts
+            acc = conlleval.calculate_accuracy(c.correct_tags, c.token_counter)
+            overall = conlleval.calculate_metrics(c.correct_chunk, c.found_guessed, c.found_correct)
+            print('end: %d sentences' % sen_count)
+            print('#sen, #token, #chunk, #chunk_pred: %d %d %d %d' %
+                  (sen_count, c.token_counter, c.found_correct, c.found_guessed))
+            print('TP, FP, FN: %d %d %d' % (overall.tp, overall.fp, overall.fn))
+            print('A, P, R, F:%6.2f %6.2f %6.2f %6.2f' % 
+                  (100.*acc, 100.*overall.prec, 100.*overall.rec, 100.*overall.fscore))
+            print()
 
-                fo.write('sen %d: %s\n' % (sen_count, sen))
-                fo.write('out: %s\n' % k_res)
-                iter = generate_lines(g_chars, g_bounds, k_bounds)
-                for token in iter:
-                    fo.write('%s\t%s\t%s\n' % (token[0], token[1], token[2]))
-                fo.write('\n')
-
-                # clear
-                sen = ''
-                g_chars = []
-                g_bounds = []
-
-            sen += word
-            g_chars.extend([word[i] for i in range(len(word))])
-            g_bounds.extend([util.get_label(i, None) for i in range(len(word))])
-
-            bof = False
+            # log
+            # sen = ''
+            # fo.write('sen %d: %s\n' % (sen_count, sen))
+            # fo.write('out: %s\n' % k_res)
+            # iter = generate_lines(g_chars, g_bounds, k_bounds)
+            # for token in iter:
+            #     fo.write('%s\t%s\t%s\n' % (token[0], token[1], token[2]))
+            # fo.write('\n')
 
     # evaluate
-
     sen_count += 1
     c = eval_counts
     acc = conlleval.calculate_accuracy(c.correct_tags, c.token_counter)
@@ -74,13 +61,7 @@ def run_and_eval_kytea(in_path, out_path):
           (100.*acc, 100.*overall.prec, 100.*overall.rec, 100.*overall.fscore))
 
 
-
-def run_kytea_for_word_segmentation(sen):
-    cmd = 'echo ' + sen + ' | kytea'
-    p = subprocess.Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-    out, err = p.communicate()
-    res = out.decode('utf-8').rstrip()
-    
+def parse_result(res):
     chars = []
     bounds = []
     for token in res.split(' '):
@@ -89,11 +70,11 @@ def run_kytea_for_word_segmentation(sen):
         # pos = feats[1]
         # read = feats[2]
 
-        # chars.extend([word[i] for i in range(len(word))])
+        chars.extend([word[i] for i in range(len(word))])
         bounds.extend([util.get_label(i, None) for i in range(len(word))])
         
-    # return chars, bounds
-    return res, bounds
+    return chars, bounds
+
 
 def generate_lines(x, t, y):
     i = 0
@@ -107,14 +88,82 @@ def generate_lines(x, t, y):
         
         i += 1
 
-            
+
+def sentence_iterator(gold_path, pred_path='', model_path=''):
+    fp = open(pred_path) if pred_path else None
+
+    print('read file:', gold_path)
+    with open(gold_path) as fg:
+        g_chars = []
+        g_bounds = []
+        
+        for line in fg:
+            line = line.rstrip()
+            g_chars, g_bounds = parse_result(line)
+
+            if fp:
+                res = fp.readline().rstrip()
+            else:
+                # run kytea
+                sen = ''
+                tokens = line.split(' ')
+                for token in tokens:
+                    sen += token.split('/')[0]
+
+                cmd = 'echo ' + sen + ' | kytea'
+                if model_path:
+                    cmd += ' -model ' + model_path
+                p = subprocess.Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+                out, err = p.communicate()
+                res = out.decode('utf-8').rstrip()
+            _, k_bounds = parse_result(res)
+
+            yield [g_chars, g_bounds, k_bounds]
+
+    if fp:
+        fp.close()
+
+    raise StopIteration
+    
+
 if __name__=='__main__':
-    dpath='/home/shigashi/data_shigashi/work/work_chainer/wordseg/bccwj_data/Disk4/processed/ma/'
-    sample_path = dpath+'LBb_train_small.tsv'
-    train_path = dpath+'LBb_train.tsv'
-    val_path = dpath+'LBb_val.tsv'
-    test_path = dpath+'LBb_test.tsv'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--preprocess', default='')
+    parser.add_argument('--dict', default='')
+    parser.add_argument('--org_data_dir', default='')
+    parser.add_argument('--train', type=int, default=-1)
+    parser.add_argument('--eval', type=int, default=-1)
+    parser.add_argument('--data_dir', '-d', default='')
+    parser.add_argument('--fileid', '-f', default='')
+    parser.add_argument('--pred_dir', '-p', default='') 
+    parser.add_argument('--out_dir', '-o', default='')
+    args = parser.parse_args()
+    print(args, '\n')
 
-    debug_path = '/home/shigashi/data_shigashi/work/work_chainer/wordseg/out_kytea/res_sample.txt'
+    # preprocess data
+    exts = ['_train.tsv', '_val.tsv']
 
-    run_and_eval_kytea(sample_path, debug_path)
+    if args.preprocess:
+        for ext in exts:
+            input = args.org_data_dir + args.fileid + ext
+            output = args.data_dir + args.fileid + ext
+            util.process_data_for_kytea(input, output)
+
+    train_gold = args.data_dir + args.fileid + '_train.tsv'
+    val_gold = args.data_dir + args.fileid + '_val.tsv'
+    test_gold = args.data_dir + args.fileid + '_test.tsv'
+    model_path = args.out_dir + args.fileid + '.mod'
+    debug_path = args.out_dir + args.fileid + '.debug'
+
+    if args.train > 0:
+        train_kytea(train_gold, model_path, args.dict)
+
+    if args.eval > 0:
+        train_pred = args.pred_dir + args.fileid + '_train.pred' if args.pred_dir else ''
+        val_pred = args.pred_dir + args.fileid + '_val.pred' if args.pred_dir else ''
+
+        # print('<training result>')
+        # eval_kytea(gold_path=train_gold, pred_path=train_pred, model_path=model_path)
+        # print()
+        print('<validation result>')
+        eval_kytea(gold_path=val_gold, pred_path=val_pred, model_path=model_path)
